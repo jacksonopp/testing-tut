a97d83ff813509a1181a97121670bc9c927f1847
Jackson Oppenheim <jackson.oppenheim@pearson.com>
2022-09-06 17:16:59 -0500

    remove old imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
