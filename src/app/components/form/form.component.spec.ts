d65cca6ed5824bb7bd79c5a4cce374689939ab7a
Jackson Oppenheim <jackson.oppenheim@pearson.com>
2022-09-07 12:08:18 -0500

    the point where we stoped the demo

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
 
 import { FormComponent } from './form.component';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let apiService: jasmine.SpyObj<ApiService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ReactiveFormsModule],
       declarations: [ FormComponent ],
+      providers: [
+        
+      ]
     })
       .compileComponents();
 
@@ -22,4 +29,23 @@ describe('FormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('succsefully submitting the form', () => {
+    it('should put the first name on the screen', () => {
+      const de = fixture.debugElement.query(By.css('[data-test="first"]'));
+      expect(de).toBeTruthy();
+
+      component.handleSubmit();
+      fixture.detectChanges();
+
+      expect((de.nativeElement as HTMLParagraphElement).innerText).toContain('test')
+    })
+  })
+
+  function createSpies() {
+    // Creating the spy
+    apiService = jasmine.createSpyObj<ApiService>('ApiService', ['submitPerson'])
+    // Creating mocks of methods
+    apiService.submitPerson.and.returnValue(of({firstName: 'test', lastName: 'testLast', id: 1}))
+  }
 });
